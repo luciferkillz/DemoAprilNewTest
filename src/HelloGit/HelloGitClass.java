baf7a8a6dee5549e33acdb96c0be1129613af0fc
luciferkillz <salmankhan14786@gmail.com>
2025-05-11 12:50:44 +0530

    This is a second testing

--- src/HelloGit/HelloGitClass.java
+++ src/HelloGit/HelloGitClass.java
@@ -3,5 +3,6 @@ package HelloGit;
 public class HelloGitClass {
 		public static void main(String[] args) {
 			System.out.println("Hello Git this is my first attempt");
+			System.out.println("This is a second testing");
 		}
 }
