418cc73fe5eeef29717f98200ac453ae140079b5
luciferkillz <salmankhan14786@gmail.com>
2025-05-11 13:29:04 +0530

    This is second commit to Git as Mentioned

--- src/HelloGit/helloGitPush.java
+++ src/HelloGit/helloGitPush.java
@@ -3,6 +3,7 @@ package HelloGit;
 public class helloGitPush {
 	public static void main(String[] args) {
 		System.out.println("Hello Git for the second time today");	
+		System.out.println("This is a second commit to git");
 	}
 	
 }
